ae0d9d35ccd1ac03fe7fad20b695491206ccf348
Lukasz Maslanka <lukasz.maslanka@gmail.com>
2014-01-31 11:21:28 -0500

    First draft of the registry library. Still need to add unit tests and documentation.

---
+++ Registry.Library/Registry.cs
@@ -0,0 +1,43 @@
+﻿namespace Apollo.Web.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Registry<T, TKey, TResult>
+    {
+        public Func<T, TResult> GetAction(TKey key)
+        {
+            Func<T, TResult> value;
+            if (actions.TryGetValue(key, out value))
+            {
+                return value;
+            }
+
+            return defaultAction;
+        }
+
+        public void Register(TKey key, Func<T, TResult> action)
+        {
+            actions.Add(key, action);
+        }
+
+        public bool Remove(TKey key)
+        {
+            return actions.Remove(key);
+        }
+
+        public void Clear()
+        {
+            actions.Clear();
+        }
+
+        public Registry(Func<T, TResult> defaultAction)
+        {
+            this.defaultAction = defaultAction;
+            actions = new Dictionary<TKey, Func<T, TResult>>();
+        }
+
+        private readonly Func<T, TResult> defaultAction;
+        private readonly Dictionary<TKey, Func<T, TResult>> actions;
+    }
+}
